03ea0334ca019269707341df8c4c33109150f033
veryyoung <codingyoung@gmail.com>
2016-09-20 23:15:40 +0800

    优化消息监听方式 #2

--- app/src/main/java/me/veryyoung/dingding/luckymoney/Main.java
+++ app/src/main/java/me/veryyoung/dingding/luckymoney/Main.java
@@ -4,7 +4,7 @@ import android.widget.ImageButton;
 
 import org.json.JSONObject;
 
-import java.lang.reflect.Field;
+import java.util.List;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -15,6 +15,7 @@ import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 import static de.robv.android.xposed.XposedHelpers.findConstructorBestMatch;
+import static de.robv.android.xposed.XposedHelpers.findField;
 import static de.robv.android.xposed.XposedHelpers.findFirstFieldByExactType;
 import static java.lang.Thread.sleep;
 
@@ -23,48 +24,46 @@ public class Main implements IXposedHookLoadPackage {
 
     private static final String DINGDING_PACKAGE_NAME = "com.alibaba.android.rimet";
 
-    private static final String MAP_CLASS_NAME = "com.alibaba.android.rimet.biz.im.notification.MessageNotificationManager";
-    private static final String MAP_FUNCTION_NAME = "a";
+    private static final String CONVERSATION_CHANGE_MAID_CLASS_NAME = "anv";
 
 
     @Override
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
         if (lpparam.packageName.equals(DINGDING_PACKAGE_NAME)) {
 
-            findAndHookMethod(MAP_CLASS_NAME, lpparam.classLoader, MAP_FUNCTION_NAME, "com.alibaba.wukong.im.Message", new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            if (!PreferencesUtils.open()) {
-                                return;
+            findAndHookMethod(CONVERSATION_CHANGE_MAID_CLASS_NAME, lpparam.classLoader, "onLatestMessageChanged", List.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    if (!PreferencesUtils.open()) {
+                        return;
+                    }
+                    List conversations = (List) param.args[0];
+                    for (Object conversation : conversations) {
+                        Object message = callMethod(conversation, "latestMessage");
+                        String messageContent = findField(message.getClass().getSuperclass().getSuperclass(), "mMessageContent").get(message).toString();
+
+                        JSONObject jsonObject = new JSONObject(messageContent);
+                        int tp = jsonObject.optInt("tp", 0);
+                        if (901 == tp || 902 == tp) {
+                            String ext = jsonObject.getJSONArray("multi").getJSONObject(0).getString("ext");
+                            ext = ext.replace("\\", "").replace("\"{", "{").replace("}\"", "}");
+                            jsonObject = new JSONObject(ext);
+                            Long sender = jsonObject.getLong("sid");
+                            String clusterId = jsonObject.getString("clusterid");
+
+                            Object redPacketsRpc = callStaticMethod(findClass("abc", lpparam.classLoader), "a");
+                            Object redPacketsRpc$9 = findConstructorBestMatch(findClass("abc$9", lpparam.classLoader), redPacketsRpc.getClass(), findClass("afm", lpparam.classLoader)).newInstance(redPacketsRpc, null);
+
+                            Object redEnvelopPickIService = callStaticMethod(findClass("cvs", lpparam.classLoader), "a", findClass("com.alibaba.android.dingtalk.redpackets.idl.service.RedEnvelopPickIService", lpparam.classLoader));
+
+                            if (PreferencesUtils.delay()) {
+                                sleep(PreferencesUtils.delayTime());
                             }
-                            if (null != param.args[0]) {
-                                Field messageContentFileld = param.args[0].getClass().getSuperclass().getSuperclass().getDeclaredField("mMessageContent");
-                                String messageContent = messageContentFileld.get(param.args[0]).toString();
-
-                                JSONObject jsonObject = new JSONObject(messageContent);
-                                int tp = jsonObject.optInt("tp", 0);
-                                if (901 == tp || 902 == tp) {
-                                    String ext = jsonObject.getJSONArray("multi").getJSONObject(0).getString("ext");
-                                    ext = ext.replace("\\", "").replace("\"{", "{").replace("}\"", "}");
-                                    jsonObject = new JSONObject(ext);
-                                    Long sender = jsonObject.getLong("sid");
-                                    String clusterId = jsonObject.getString("clusterid");
-
-                                    Object redPacketsRpc = callStaticMethod(findClass("abc", lpparam.classLoader), "a");
-                                    Object redPacketsRpc$9 = findConstructorBestMatch(findClass("abc$9", lpparam.classLoader), redPacketsRpc.getClass(), findClass("afm", lpparam.classLoader)).newInstance(redPacketsRpc, null);
-
-                                    Object redEnvelopPickIService = callStaticMethod(findClass("cvs", lpparam.classLoader), "a", findClass("com.alibaba.android.dingtalk.redpackets.idl.service.RedEnvelopPickIService", lpparam.classLoader));
-
-                                    if (PreferencesUtils.delay()) {
-                                        sleep(PreferencesUtils.delayTime());
-                                    }
-                                    callMethod(redEnvelopPickIService, "pickRedEnvelopCluster", sender, clusterId, redPacketsRpc$9);
-                                }
-                            }
+                            callMethod(redEnvelopPickIService, "pickRedEnvelopCluster", sender, clusterId, redPacketsRpc$9);
                         }
                     }
-            );
-
+                }
+            });
 
             findAndHookMethod("com.alibaba.android.dingtalk.redpackets.activities.PickRedPacketsActivity", lpparam.classLoader, "a", new XC_MethodHook() {
                 @Override
