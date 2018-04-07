25b21639ea0631d206c3977f4f1f134dbbf55cba
veryyoung <codingyoung@gmail.com>
2018-04-07 16:26:41 +0800

    Support dingding 4.3.3

--- app/src/main/java/me/veryyoung/dingding/luckymoney/VersionParam.java
+++ app/src/main/java/me/veryyoung/dingding/luckymoney/VersionParam.java
@@ -6,10 +6,10 @@ package me.veryyoung.dingding.luckymoney;
 public class VersionParam {
 
     public static String ApiEventListener = "bve";
-    public static String ConversationChangeMaid = "cns";
-    public static String ServiceFactory = "gse";
+    public static String ConversationChangeMaid = "cnx";
+    public static String ServiceFactory = "gug";
     public static String RedPacketsRpc = "bnf";
-    public static String MessageDs = "fzm";
+    public static String MessageDs = "gbm";
 
     public static void init(String version) {
         switch (version) {
@@ -190,12 +190,19 @@ public class VersionParam {
                 RedPacketsRpc = "bnf";
                 MessageDs = "fzm";
                 break;
+            case "4.3.3":
+                ApiEventListener = "bve";
+                ConversationChangeMaid = "cnx";
+                ServiceFactory = "gug";
+                RedPacketsRpc = "bnf";
+                MessageDs = "gbm";
+                break;
             default:
                 ApiEventListener = "bve";
-                ConversationChangeMaid = "cns";
-                ServiceFactory = "gse";
+                ConversationChangeMaid = "cnx";
+                ServiceFactory = "gug";
                 RedPacketsRpc = "bnf";
-                MessageDs = "fzm";
+                MessageDs = "gbm";
         }
     }
 
